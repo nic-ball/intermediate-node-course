931a6cf767d5edaec8eb29d191f9991256ac3c6c
nic-ball <nicholasball147@gmail.com>
2020-10-12 14:14:17 +0100

    add user model and connect to db

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const bodyParser= require('body-parser');
 const port=8000;
 const app= express();
 
+const User = require('./models/User');
+mongoose.connect('mongodb://localhost/userData')
+
 app.use(bodyParser.json());
 
 app.listen(port, ()=>{
